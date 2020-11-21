768d281cb57295c239c2f441c498175ad1707958
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-11-20 19:20:42 -0500

    Stage 3 of 7 for JetBrains Academy - Kotlin - Git Internals project. • this stage has us handle blob and commit git files, where if it is a blob the contents is to be printed out, and if it's a commit, then the first few lines have to be formatted to specifications, and the time stamps have to printed in a readable format. • main() - was adjusted to print out the header. Then loop through the contents of the inflated file. If the file is type blob it just prints out each line, if it's a commit file, it formats the first few lines to specification, and then once it gets to the commit message part it prints out each line without modification. For the splitting of the header and first line I switched to 0.toChar() instead of '\u0000', as I found it was unable to split my personal git files ( though it was able to split the test git files for the stages ). Also for the retrieving of the file the folder path "objects" was added ( it was also added to last stage, but I forgot to commit it ). • formatLine() - was added to handle the formatting of lines that start with "author" and "commit". Removing the "<" and ">" surrounding email addresses, formatting the time, and adding "original" for line that starts with "author" and "commit" for line that starts with "commit" plus "timestamp:" to both after the email address and before the formatted time.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,63 @@
 import java.io.FileInputStream
 import java.io.FileNotFoundException
+import java.text.SimpleDateFormat
 import java.util.zip.InflaterInputStream
+import java.util.TimeZone
 
 fun main() {
     try {
         val directory = getString("Enter .git directory location:")
         val gitHash = getString("Enter git object hash:")
-        val gitFile = FileInputStream("$directory/${gitHash.substring(0, 2)}/${gitHash.substring(2)}")
-        val header = InflaterInputStream(gitFile).reader().readLines()[0].split('\u0000')[0].split(" ")
+        val gitFile = FileInputStream("$directory/objects/${gitHash.substring(0, 2)}/${gitHash.substring(2)}")
+        val inflatedFile = InflaterInputStream(gitFile).reader().readLines()
+        val headerFirst = inflatedFile[0].split(0.toChar())
+        val header = headerFirst[0].split(" ")[0].toUpperCase()
+        var commitMessage = false
 
-        println("type:${header[0]} length:${header[1]}")
+        println("*$header*")
+        for (i in inflatedFile.indices) {
+            if (header == "BLOB" || commitMessage) {
+                println(if (i == 0) headerFirst[1] else inflatedFile[i])
+            } else {
+                if (i != 0 && inflatedFile[i] == "") {
+                    commitMessage = true
+                    if (i != inflatedFile.lastIndex) println("commit message:")
+                } else {
+                    var line = (if (i == 0) headerFirst[1] else inflatedFile[i]).replaceFirst(" ", ": ")
+
+                    when (val type = line.substring(0, 6)) {
+                        "parent" -> line = line.replace("parent", "parents")
+                        "author", "commit" -> line = formatLine(line, type)
+                    }
+                    println(line)
+                }
+            }
+        }
         gitFile.close()
     } catch (e: FileNotFoundException) {
         println("File not found.\n")
     } catch (e: Exception) {
         println("There was an error in loading your file. Please ensure it is not open in another program.\n")
     }
+}
+
+fun formatLine(lineStr: String, type: String): String {
+    var line = ""
+    val hold = lineStr.split(" ").toMutableList()
+    val gmtString = hold[hold.lastIndex].substring(0, 3) + ":" + hold[hold.lastIndex].substring(3)
+    val addWords = (if (type == "author") " original" else " commit") + " timestamp:"
+    val formatTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val gmtNum = (gmtString.substring(0, 3).toInt() * 3600) +
+            ((gmtString.substring(0, 1) + gmtString.substring(4)).toInt() * 60)
+    val timeNum = (hold[hold.lastIndex - 1].toLong() + gmtNum) * 1000L
+
+    formatTime.timeZone = TimeZone.getTimeZone("GMT")
+    hold[hold.lastIndex] = gmtString
+    hold[hold.lastIndex - 1] = formatTime.format(timeNum)
+    hold.forEach { line += "$it " }
+    line = line.replace("<", "").replace(">", addWords)
+
+    return line.trim()
 }
 
 fun getString(text: String): String {
