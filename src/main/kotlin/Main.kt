5cf96168b6bedfc0ffc35150a8f4934b8c0874fe
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-11-22 16:51:32 -0500

    Stage 4 of 7 for JetBrains Academy - Kotlin - Git Internals project. • this stage has us handle tree git files in addition to commit and blob. • main() - val gitFile was adjusted to lambda so that the new function handleTree() can be passed a new FileInputStream of the same file for when the git file is a Tree type. And an if statement was added to check to see if the header is TREE and if so it calls handleTree otherwise it proceeds as normal. The line with gitFile.close() was removed as it's now a lambda. • handTree() - it inflates the passed FileInputStream and then loops through each byte. Once the header has been passed it adds the numbers to var combined and then saves the file/folder name to var lastWord. Once it hits a null char it then converts the next twenty bytes to hex and saves it into var combined. Once it has read twenty bytes it then adds the lastWord to combined and then resets the var count, byteCound, and lastWord. It then repeats the cycle if there are any more tree lines to be added until it loops through all the val inflatedBytes bytes. Once the cycles are complete combined is then printed and the file is closed. • I had the file read twice ( once in main() and then again in handleTree() ), because doing .reader().readLines() in main() does a better job of converting special characters in commit messages and blobs then byte.toChar() does ( but I couldn't simply use the read lines in main() for handleTree() because the conversion loses some of the byte details that are needed to convert the SHA-1 bytes into hex formatting ).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,32 +8,33 @@ fun main() {
     try {
         val directory = getString("Enter .git directory location:")
         val gitHash = getString("Enter git object hash:")
-        val gitFile = FileInputStream("$directory/objects/${gitHash.substring(0, 2)}/${gitHash.substring(2)}")
-        val inflatedFile = InflaterInputStream(gitFile).reader().readLines()
+        val gitFile = { FileInputStream("$directory/objects/${gitHash.substring(0, 2)}/${gitHash.substring(2)}") }
+        val inflatedFile = InflaterInputStream(gitFile()).reader().readLines()
         val headerFirst = inflatedFile[0].split(0.toChar())
         val header = headerFirst[0].split(" ")[0].toUpperCase()
         var commitMessage = false
 
         println("*$header*")
-        for (i in inflatedFile.indices) {
-            if (header == "BLOB" || commitMessage) {
-                println(if (i == 0) headerFirst[1] else inflatedFile[i])
-            } else {
-                if (i != 0 && inflatedFile[i] == "") {
-                    commitMessage = true
-                    if (i != inflatedFile.lastIndex) println("commit message:")
+        if (header == "TREE") handleTree(gitFile()) else {
+            for (i in inflatedFile.indices) {
+                if (header == "BLOB" || commitMessage) {
+                    println(if (i == 0) headerFirst[1] else inflatedFile[i])
                 } else {
-                    var line = (if (i == 0) headerFirst[1] else inflatedFile[i]).replaceFirst(" ", ": ")
+                    if (i != 0 && inflatedFile[i] == "") {
+                        commitMessage = true
+                        if (i != inflatedFile.lastIndex) println("commit message:")
+                    } else {
+                        var line = (if (i == 0) headerFirst[1] else inflatedFile[i]).replaceFirst(" ", ": ")
 
-                    when (val type = line.substring(0, 6)) {
-                        "parent" -> line = line.replace("parent", "parents")
-                        "author", "commit" -> line = formatLine(line, type)
+                        when (val type = line.substring(0, 6)) {
+                            "parent" -> line = line.replace("parent", "parents")
+                            "author", "commit" -> line = formatLine(line, type)
+                        }
+                        println(line)
                     }
-                    println(line)
                 }
             }
         }
-        gitFile.close()
     } catch (e: FileNotFoundException) {
         println("File not found.\n")
     } catch (e: Exception) {
@@ -58,6 +59,40 @@ fun formatLine(lineStr: String, type: String): String {
     line = line.replace("<", "").replace(">", addWords)
 
     return line.trim()
+}
+
+fun handleTree(gitFile: FileInputStream) {
+    val inflatedBytes = InflaterInputStream(gitFile).readAllBytes()
+    var combined = ""
+    var lastWord = ""
+    var count = 0
+    var byteCount = 0
+    var passedHeader = false
+
+    for (byte in inflatedBytes) {
+        if (passedHeader) {
+            when (count) {
+                0 -> {
+                    if (byte.toChar() == ' ') count++
+                    combined += "${byte.toChar()}"
+                }
+                1 -> if (byte.toChar() == 0.toChar()) count++ else lastWord += "${byte.toChar()}"
+                2 -> {
+                    byteCount++
+                    combined += String.format("%X", byte).toLowerCase()
+                    if (byteCount == 20) {
+                        combined += " $lastWord\n"
+                        lastWord = ""
+                        count = 0
+                        byteCount = 0
+                    }
+                }
+            }
+        }
+        if (!passedHeader && byte.toChar() == 0.toChar()) passedHeader = true
+    }
+    print(combined)
+    gitFile.close()
 }
 
 fun getString(text: String): String {
